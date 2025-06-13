8de9d6c1be2f3ff189a30d3135bbd3dc72aaf227
mmahnoosh <montazer_mahnoosh@gmx.de>
2025-06-13 16:17:48 +0200

    Initial commit

---
+++ data_models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, DATE
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = 'Author'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    birth_date = Column(DATE, nullable=False)
+    date_of_death = Column(DATE, nullable=True)
+
+    def __repr__(self):
+        return (f"id: {self.id} name:{self.name} birth_date:{self.birth_date} "
+                f"date_of_death:{self.date_of_death}")
+
+    def __str__(self):
+        if not self.date_of_death:
+            return f"name:{self.id} birth_date:{self.birth_date}"
+        return f"name:{self.id} birth_date:{self.birth_date} date_of_death:{self.date_of_death}"
+
+
+class Book(db.Model):
+    __tablename__ = 'Book'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    isbn = Column(String(20))
+    title = Column(String(100))
+    publication_year = Column(Integer)
+    author_id = Column(Integer)
+
+    def __repr__(self):
+        return (f"id: {self.id} isbn_: {self.isbn} title:{self.title} "
+                f"publication_year:{self.publication_year} author_id:{self.author_id}")
+
+    def __str__(self):
+        return (f"isbn_: {self.isbn} title:{self.title} "
+                f"publication_year:{self.publication_year}")
